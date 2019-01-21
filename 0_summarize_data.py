60210d1a0068b6de3cb6864b8e1a4a4c8044d84d
Konstantin Barsukov <konstantin.barsukov@novartis.com>
2019-01-21 15:28:20 +0000

    Fix "/" issue

--- 0_summarize_data.py
+++ 0_summarize_data.py
@@ -176,7 +176,7 @@ def main():
 
 
     #if no processed expression present, runs cuffquant/cuffnorm/RNA-seq pipeline
-    cufflinksFolder = utils.formatFolder('%s/cufflinks' % (projectFolder),True)
+    cufflinksFolder = utils.formatFolder('%scufflinks' % (projectFolder),True)
     analysis_name = 'NIBR_YvsO'
     young_rna_list = config.get_global_name('young_rna_list')
     old_rna_list = config.get_global_name('old_rna_list')
